bb7dbee19539a98825853eb1d6db725325cedbdd
Dorian Birch <206dorian@gmail.com>
2023-01-23 14:27:57 -0600

    debugged ContactMe

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -38,7 +38,7 @@ function Navigation({ currentPage, handlePageChange }) {
       </li>
       <li className="nav-item">
         <a
-          href=".ContactMe"
+          href="#ContactMe"
           onClick={() => handlePageChange('ContactMe')}
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'ContactMe' ? 'nav-link active' : 'nav-link'}
